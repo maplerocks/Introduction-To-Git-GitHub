d66ca7fc6b17a2ff76f63180fe9bcf93cda750b7
sammyiscoding <sammyiscoding@gmail.com>
2025-10-19 23:00:20 +0800

    Added comments in main.py highlighting more detail on the code line 2

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 print("Hello World From Snoopy")
+# This line was added by user JacklyneC who is not a contributor but forked the repo and submit as a contribution
 print("Message from JacklyneC - adding a new line.")
