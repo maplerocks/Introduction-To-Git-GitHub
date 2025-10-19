16a6788841feb5202bef29208fd5ec3b5f49918c
sammyiscoding <sammyiscoding@gmail.com>
2025-10-19 23:09:45 +0800

    added new resource pdf

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello World From Snoopy")
 # This line was added by user JacklyneC who is not a contributor but forked the repo and submit as a contribution
 print("Message from JacklyneC - adding a new line.")
+
