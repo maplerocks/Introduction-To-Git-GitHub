848273b455f48a74769abdf4f1356fe56516c82c
sammyiscoding <sammyiscoding@gmail.com>
2025-10-21 15:07:43 +0800

    Added test line 5 in TestBranch

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ print("Hello World From Snoopy")
 # This line was added by user JacklyneC who is not a contributor but forked the repo and submit as a contribution
 print("Message from JacklyneC - adding a new line.")
 # This comment here is a separate push to visually see how commit messages are displayed in Git
+print("Adding information on test branch to be merge to main branch")
